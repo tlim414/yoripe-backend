f8d38023f912bf803d53d208a0da0b03b8ee472b
Ted Lim <tedlim@Teds-MacBook-Pro.local>
2026-06-05 12:04:37 -0400

    fix prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import "dotenv/config"; // Load env variable first
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const prisma = new PrismaClient({ adapter });
